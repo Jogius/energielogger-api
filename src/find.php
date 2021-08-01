02b236de7fd1ad800313d91b8f4172163b3a7744
Jogius <jogius.julius.makowski@gmail.com>
2021-08-01 13:58:02 +0200

    `from` and `to` query parameters

--- src/find.php
+++ src/find.php
@@ -15,20 +15,45 @@ $dotenv->load();
 // Initialize Database connection
 $dbConnection = (new DatabaseConnector())->getConnection();
 
-$query = "SELECT * FROM data ORDER BY timestamp ASC;";
-
-$statement = $dbConnection->query($query);
-
-$success = $statement->execute();
-
-if ($success)
-{
-  $data = $statement->fetchAll();
-
-  echo json_encode($data);
+// Check for `from` and `to` query parameters
+$params = array();
+parse_str($_SERVER["QUERY_STRING"], $params);
+if (
+  !empty($params["from"]) &&
+  !empty($params["to"])
+) {
+  $range = TRUE;
+  $from = $params["from"];
+  $to = $params["to"];
+} else {
+  $range = FALSE;
 }
-else
-{
-  http_response_code(403);
-  echo json_encode(array("message" => "Internal error."));
+
+
+try {
+  if ($range) {
+    $query = "SELECT * FROM data WHERE timestamp BETWEEN FROM_UNIXTIME(:from) AND FROM_UNIXTIME(:to) ORDER BY timestamp ASC;";
+
+    $statement = $dbConnection->prepare($query);
+    $statement->bindParam(":from", $from);
+    $statement->bindParam(":to", $to);
+  } else {
+    $query = "SELECT * FROM data ORDER BY timestamp ASC;";
+
+    $statement = $dbConnection->query($query);
+  }
+
+  $success = $statement->execute();
+
+  if ($success) {
+    $data = $statement->fetchAll();
+
+    echo json_encode($data);
+  } else {
+    http_response_code(403);
+    echo json_encode(array("message" => "Internal error."));
+  }
+} catch (PDOException $e) {
+  http_response_code(400);
+  echo json_encode(array("message" => $e->getMessage()));
 }
